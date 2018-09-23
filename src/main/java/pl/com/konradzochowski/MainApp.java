3567b8fdf0ad6c0cae0f45d9025b723df64ee976
Konrad Żochowski <zochowski.konrad1@gmail.com>
2018-09-24 01:28:24 +0200

    initial commit

---
+++ src/main/java/pl/com/konradzochowski/MainApp.java
@@ -0,0 +1,12 @@
+package pl.com.konradzochowski;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        HelloWorld helloWorld = (HelloWorld) context.getBean("helloWorld");
+        System.out.println(helloWorld.getMessage());
+    }
+}
